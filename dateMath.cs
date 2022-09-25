327ea983490fa825c8221936d1f34e8be6605a61
bbeligte <111139410+bbeligte@users.noreply.github.com>
2022-09-25 23:38:51 +0300

    Add files via upload

---
+++ dateMath.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double yearOfDeath = 0;
+        double timeLeft = 0;
+
+        Console.WriteLine("Lütfen doğduğunuz yılı giriniz: ");
+        int by = int.Parse(Console.ReadLine());
+        Console.WriteLine("Lütfen biyolojik cinsiyetiniz erkek ise 1, kadın ise 2'yi tuşlayınız: ");
+        int sex = int.Parse(Console.ReadLine());
+
+        int year = DateTime.Now.Year;
+
+        if(sex == 1)
+        {
+            yearOfDeath = Math.Round(75.6) - (year-by) + year;
+            timeLeft =  (yearOfDeath - year) * 8766;
+        }
+        else
+        {
+            yearOfDeath = Math.Ceiling(81.2) - (year-by) + year;
+            timeLeft =  (yearOfDeath - year) * 8766;
+        }
+            
+        Console.WriteLine("Öleceğinizi tahmin ettiğimiz yıl: ");
+        Console.WriteLine(yearOfDeath);
+        Console.WriteLine("Kalan, yaklaşık, saatleriniz: ");
+        Console.Write(timeLeft.ToString().Insert(3, "."));
+    }
+}

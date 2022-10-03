8454f5dae82d4bca58c6c213feca9cf14d1a60c1
bbeligte <111139410+bbeligte@users.noreply.github.com>
2022-10-03 23:07:06 +0300

    Koleksiyonlar-Soru-3.cs
    Koleksiyonlar-Soru-3

---
+++ colHW3.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Koleksiyonlar soru 3
+
+        var sesliler = new HashSet<char> {'a','e', 'ı', 'i', 'o', 'ö', 'u', 'ü'};
+        List<char> sHarfler = new List<char>();
+
+        Console.WriteLine("Bir cümle giriniz: ");
+
+        string s = Console.ReadLine().ToLower();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if(sesliler.Contains(s[i]))
+            {
+                sHarfler.Add(s[i]);
+            }    
+        }
+
+        sHarfler.Sort();
+        
+        foreach (var c in sHarfler)
+        {
+            Console.Write(c);
+        }
+    }
+}

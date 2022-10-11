9ce5d247e73d2cd2a36faf6a03d55bf957ebcd30
bbeligte <111139410+bbeligte@users.noreply.github.com>
2022-10-11 21:48:58 +0300

    Add files via upload

---
+++ Enum.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hava sıcaklığını giriniz: ");
+
+        int sicaklik = int.Parse(Console.ReadLine());
+
+        if (sicaklik <= (int)Havadurumu.soguk)
+            Console.WriteLine("Hava soğuk");
+        else if (sicaklik >= (int)Havadurumu.iyi && sicaklik < (int)Havadurumu.sicak )
+            Console.WriteLine("Hava güzel");
+        else
+            Console.WriteLine("Hava sıcak");
+    }
+
+    enum Havadurumu
+    {
+        soguk = 14,
+        iyi = 15,
+        sicak = 30
+    }
+}
